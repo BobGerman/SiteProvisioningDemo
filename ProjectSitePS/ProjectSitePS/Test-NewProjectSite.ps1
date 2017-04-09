f21119d6d782de935ba81ac96ed8ce35da397af6
BobGerman <bgerman1@hotmail.com>
2017-04-09 18:55:11 -0500

    Updated for new PnP commands (changed Nov 2016)

--- ProjectSitePS/ProjectSitePS/Test-NewProjectSite.ps1
+++ ProjectSitePS/ProjectSitePS/Test-NewProjectSite.ps1
@@ -3,17 +3,17 @@
 #
 
 if ($me -eq $null) {
-	$me = Get-Credential -UserName "bobg@bgtest14.onmicrosoft.com" -Message "Enter your credentials"
+	$me = Get-Credential -UserName "admin@a830edad9050849canary022828.onmicrosoft.com" -Message "Enter your credentials"
 }
-$testNumber = "27"
+$testNumber = "10"
 
 
 .\New-ProjectSite.ps1 -Title "Project site $testNumber" `
                              -Credentials $me `
-                             -Url "https://bgtest14.sharepoint.com/sites/DemoTest$testNumber" `
-							 -Folder "C:\Users\bob\Source\Repos\Demos15\ProvisioningDemo\ProjectSitePS\ProjectSitePS" `
+                             -Url "https://a830edad9050849canary022828.sharepoint.com/sites/t$testNumber" `
+							 -Folder "C:\Users\bob\Source\Repos\SiteProvisioningDemo\ProjectSitePS\ProjectSitePS" `
 							 -LogFile "C:\Users\bob\Documents\Test\TestLog$testNumber.txt" `
 							 -Description "Demo Site $testNumber" `
-							 -SiteOwner1 "bobg@bgtest14.onmicrosoft.com" `
-							 -SiteOwner2 "biz@bgtest14.onmicrosoft.com"
+							 -SiteOwner1 "admin@a830edad9050849canary022828.onmicrosoft.com" `
+							 -SiteOwner2 "user0@a830edad9050849canary022828.onmicrosoft.com"
 
